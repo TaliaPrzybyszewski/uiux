5f40eea56869a92a20e71ce0fd5a725d0f3fabe3
Talia Przybyszewski <tprzybyszewski@icloud.com>
2024-03-19 21:50:10 +0000

    inital commit

---
+++ components/BasicTailwindLayout.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+export default function BasicTailwindLayout() {
+  return (
+    <section>
+      <div></div>
+      <header>
+        <h1>Crystal Math</h1>
+        <button>
+          <p>Book Now</p>
+        </button>
+      </header>
+      <main>
+        <section>
+          <h2>Pure Math Made Crystal Clear</h2>
+          <p>
+            Experience the euphoric high of greater knowledge, deeper
+            understanding and some wild math 🔥{' '}
+          </p>
+          <div>
+            <button>
+              <p>Book now</p>
+            </button>
+            <button>
+              <p>Get in touch</p>
+            </button>
+          </div>
+          <div>
+            <p>4.6</p>
+            <div>
+              <i></i>
+              <i></i>
+              <i></i>
+              <i></i>
+              <div>
+                <div>
+                  <i></i>
+                </div>
+                <i></i>
+              </div>
+            </div>
+            <p>(250+)</p>
+          </div>
+        </section>
+      </main>
+      <footer>
+        <p>Made with 💛</p>
+      </footer>
+    </section>
+  )
+}
